f737a616ee3e0feff94255fe6d704ef5d0bf181b
Aummii Desu <74613199+Phornhathai@users.noreply.github.com>
2024-01-10 10:51:45 +0700

    update package

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import * as serviceWorker from "./serviceworker";
+import b2cauth from "react-azure-b2c";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+b2cauth.initialize({
+  // instance: "https://login.microsoftonline.com/tfp/",
+  // tenant: "peachitad.onmicrosoft.com",
+  // signInPolicy: "B2C_1_react_signup",
+  // applicationId: "702f55a3-72fd-4da1-a22b-1dc82e70a049",
+  // cacheLocation: "sessionStorage",
+  // scopes: ["https://peachitad.onmicrosoft.com/api/user_impersonation"],
+  // redirectUri: "http://localhost:3000",
+  // postLogoutRedirectUri: window.location.origin,
+  instance: "https://auditmypayrollv1.b2clogin.com/",
+  tenant: "auditmypayrollv1.onmicrosoft.com",
+  signInPolicy: "B2C_1_SignUpandSignIn",
+  applicationId: "293a7bda-7560-4de6-89f8-e6876881e582",
+  cacheLocation: "sessionStorage",
+  scopes: ["https://auditmypayrollv1.onmicrosoft.com/api/question.read"],
+  redirectUri: "http://localhost:3000",
+  postLogoutRedirectUri: window.location.origin,
+});
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+b2cauth.run(() => {
+  ReactDOM.render(<App />, document.getElementById("root"));
+  serviceWorker.unregister();
+});
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
